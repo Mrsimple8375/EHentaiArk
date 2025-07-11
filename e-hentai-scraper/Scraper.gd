845f7eecf0e0b765b1b125dcd680523f6b920450
Polyworld4253 <111020906+Polyworld4253@users.noreply.github.com>
2025-07-11 13:41:49 -0400

    comicinfo xml parse

--- e-hentai-scraper/Scraper.gd
+++ e-hentai-scraper/Scraper.gd
@@ -2,13 +2,20 @@ extends Node
 
 var selectedMangaFile : String
 var selectedMangaFilePath : String
-@onready var progress = $Panel2/Progress
-@onready var eHentaiURL = $"E-Hentai URL"
+
+var selectedComicInfoFile : String
+var selectedComicInfoFilePath : String
+
+@onready var progress = $"Progress Panel/Text"
+@onready var eHentaiURL = $"Crawl From URL/E-Hentai URL"
 @onready var hentaiName = $"Manga Name"
+@onready var mangaFileDialog = $"Manga FileDialog"
+@onready var comicInfoFileDialog = $"Crawl From ComicInfo/ComicInfo FileDialog"
+
+var crawlMode : String = "URL"
 
 func _ready():
 	$CookieHTTPRequest.request_completed.connect(_on_request_completed)
-
 
 func _on_request_completed(result, response_code, headers, body):
 	progress.text += "Parsed from URL:\n"
@@ -90,7 +97,8 @@ func WriteParsedIntoZipFile(_parsed : String):
 	var _dataOverwrite : PackedByteArray
 	for _file in _reader.get_files():
 		# remove existed data.txt file
-		if(_file != "data.txt"):
+		var _deleteFiles = ["data.txt", "ComicInfo.xml", ".ehviewer", ".thumb"]
+		if(_deleteFiles.has(_file) == false):
 			_filesInsideZip[_file] = _reader.read_file(_file)
 	_reader.close()
 
@@ -117,31 +125,129 @@ func WriteParsedIntoZipFile(_parsed : String):
 	progress.text += "Done."
 	return OK
 
-func _on_file_dialog_file_selected(path):
-	selectedMangaFile = $FileDialog.current_file
+func _on_manga_file_dialog_file_selected(path):
+	selectedMangaFile = mangaFileDialog.current_file
 	selectedMangaFilePath = path
 	
-	progress.text = "Manga Selected;\n" + $FileDialog.current_file + "\n\n"
+	progress.text = "Manga Selected;\n" + mangaFileDialog.current_file + "\n\n"
 	
 	eHentaiURL.text = ""
 	hentaiName.text = ""
 
+func _on_comicinfo_file_dialog_file_selected(path: String) -> void:
+	selectedComicInfoFile = comicInfoFileDialog.current_file
+	selectedComicInfoFilePath = path
+	
+	progress.text = "ComicInfo Selected;\n" + comicInfoFileDialog.current_file + "\n\n"
+	
+	hentaiName.text = ""
+
 func _on_select_manga_file_pressed():
-	$FileDialog.visible = true
+	mangaFileDialog.visible = true
+
+func _on_select_comic_info_file_pressed() -> void:
+	comicInfoFileDialog.visible = true
 
 func _on_start_pressed():
-	progress.text = "Manga Selected;\n" + $FileDialog.current_file + "\n\n"
-	
-	if(hentaiName.text == ""):
-		progress.text = "ERROR: Manga name is required.\n"
-	elif(eHentaiURL.text == ""):
-		progress.text = "ERROR: E-Hentai URL is required.\n"
-	elif(selectedMangaFile == ""):
-		progress.text = "ERROR: Manga file must be selected.\n"
-	elif(eHentaiURL.text.contains("https://e-hentai.org/g/") == false && eHentaiURL.text.contains("https://exhentai.org/g/") == false):
-		progress.text = "ERROR: Entered URL is not E-Hentai URL.\n"
-	else:
-		progress.text += "Scrapping from E-Hentai...\n" + eHentaiURL.text + "\n\n"
+	if(crawlMode == "URL"):
+		progress.text = "Manga Selected;\n" + mangaFileDialog.current_file + "\n\n"
 		
-		var _exHentaiCookies = ["ipb_member_id: " + $ipb_member_id.text, "ipb_pass_hash: " + $ipb_pass_hash.text]
-		$CookieHTTPRequest.request(eHentaiURL.text, _exHentaiCookies)
+		if(hentaiName.text == ""):
+			progress.text = "ERROR: Manga name is required.\n"
+		elif(eHentaiURL.text == ""):
+			progress.text = "ERROR: E-Hentai URL is required.\n"
+		elif(selectedMangaFile == ""):
+			progress.text = "ERROR: Manga file must be selected.\n"
+		elif(eHentaiURL.text.contains("https://e-hentai.org/g/") == false && eHentaiURL.text.contains("https://exhentai.org/g/") == false):
+			progress.text = "ERROR: Entered URL is not E-Hentai URL.\n"
+		else:
+			progress.text += "Scrapping from E-Hentai...\n" + eHentaiURL.text + "\n\n"
+			
+			$CookieHTTPRequest.cookie_request(eHentaiURL.text, [])
+	elif(crawlMode == "ComicInfo"):
+		var _parsed = ""
+		
+		var _parser = XMLParser.new()
+		_parser.open(selectedComicInfoFilePath)
+		
+		var _test : Dictionary
+		var _neededNodes = ["Penciller", "Genre", "Characters", "Teams", "Web"]
+		var _foundNeededNode : String
+		
+		while(_parser.read() != ERR_FILE_EOF):
+			var _nodeName = _parser.get_node_name()
+			var _nodeData = _parser.get_node_data()
+			
+			if(_parser.get_node_type() == 1):
+				if(_neededNodes.has(_nodeName)):
+					_foundNeededNode = _nodeName
+			elif(_parser.get_node_type() == 3):
+				if(_foundNeededNode != ""):
+					_test[_foundNeededNode] = _nodeData
+					_foundNeededNode = ""
+		
+		progress.text += "Parsed from ComicInfo:\n"
+		
+		progress.text += "\t- url: " + _test["Web"] + "\n"
+		_parsed += "url: " + _test["Web"] + "\n"
+		
+		progress.text += "\t- name: " + hentaiName.text + "\n"
+		_parsed += "name: " + hentaiName.text + "\n"
+		
+		var _languageTags = "language: korean; "
+		
+		var _parodyTags = "parody: "
+		if(_test.has("Teams") == true):
+			_parodyTags += _test["Teams"].replace(",", ";") + "; "
+		
+		var _characterTags = "character: "
+		if(_test.has("Teams") == true):
+			_characterTags += _test["Characters"].replace(",", ";") + "; "
+			
+		var _artistTags = "artist: " + _test["Penciller"].replace(",", ";") + "; "
+		
+		var _tagSplit = _test["Genre"].split(", ")
+	
+		var _maleTags = "male: "
+		var _femaleTags = "female: "
+		var _mixedTags = "mixed: "
+		var _otherTags = "other: "
+		
+		for _tag in _tagSplit:
+			if(_tag.begins_with("m:")):
+				_maleTags += _tag.replace("m:", "") + "; "
+			if(_tag.begins_with("f:")):
+				_femaleTags += _tag.replace("f:", "") + "; "
+			if(_tag.begins_with("x:")):
+				_mixedTags += _tag.replace("x:", "") + "; "
+			if(_tag.contains(":") == false):
+				_otherTags += _tag + "; "
+				
+		progress.text += "\t- " + _languageTags + "\n"
+		_parsed += _languageTags + "\n"
+		progress.text += "\t- " + _parodyTags + "\n"
+		_parsed += _parodyTags + "\n"
+		progress.text += "\t- " + _characterTags + "\n"
+		_parsed += _characterTags + "\n"
+		progress.text += "\t- " + _artistTags + "\n"
+		_parsed += _artistTags + "\n"
+		progress.text += "\t- " + _maleTags + "\n"
+		_parsed += _maleTags + "\n"
+		progress.text += "\t- " + _femaleTags + "\n"
+		_parsed += _femaleTags + "\n"
+		progress.text += "\t- " + _mixedTags + "\n"
+		_parsed += _mixedTags + "\n"
+		progress.text += "\t- " + _otherTags + "\n\n"
+		_parsed += _otherTags
+		
+		WriteParsedIntoZipFile(_parsed)
+
+func switch_to_url_crawl() -> void:
+	crawlMode = "URL"
+	$"Crawl From ComicInfo".hide()
+	$"Crawl From URL".show()
+
+func switch_to_comic_info() -> void:
+	crawlMode = "ComicInfo"
+	$"Crawl From ComicInfo".show()
+	$"Crawl From URL".hide()
